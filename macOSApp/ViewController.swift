99ee8c18da8cbaac1144ad2ef2d1ac87e2af289f
MarceloBRAmaral <marceloamaral.amaral@gmail.com>
2022-01-26 16:03:03 -0500

    Initial commit

---
+++ macOSApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  macOSApp
+//
+//  Created by Marcelo on 2022-01-23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
